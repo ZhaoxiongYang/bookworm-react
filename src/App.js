88cb6ffcdaeb20ea1daf9a54a03c2ed3f2ae66dd
Xiong <xiong@ruis-mbp.attlocal.net>
2019-06-02 01:32:01 -0700

    set up user route

--- src/App.js
+++ src/App.js
@@ -1,18 +1,70 @@
-import React from 'react';
-import { Route } from 'react-router-dom';
-import HomePage from './components/pages/HomePage';
-import LoginPage from './components/pages/LoginPage';
-import DashboardPage from './components/pages/DashboardPage';
+import React from "react";
+import { connect } from "react-redux";
+import PropTypes from "prop-types";
+import { Route } from "react-router-dom";
+import HomePage from "./components/pages/HomePage";
+import LoginPage from "./components/pages/LoginPage";
+import DashboardPage from "./components/pages/DashboardPage";
+import UserRoute from "./components/routes/UserRoute";
+// import SignupPage from "./components/pages/SignupPage";
+// import ConfirmationPage from "./components/pages/ConfirmationPage";
+// import ForgotPasswordPage from "./components/pages/ForgotPasswordPage";
+// import ResetPasswordPage from "./components/pages/ResetPasswordPage";
+// import NewBookPage from "./components/pages/NewBookPage";
+// import GuestRoute from "./components/routes/GuestRoute";
+// import TopNavigation from "./components/navigation/TopNavigation";
 
+const App = () => (
+  <div className="ui container">{/*
+    {isAuthenticated && <TopNavigation />}*/}
+    <Route path="/" exact component={HomePage} />
+    {/*<Route
+      location={location}
+      path="/confirmation/:token"
+      exact
+      component={ConfirmationPage}
+    />*/}
+    <Route  path="/login" exact component={LoginPage} />
+    {/*<GuestRoute
+      location={location}
+      path="/signup"
+      exact
+      component={SignupPage}
+    />
+    <GuestRoute
+      location={location}
+      path="/forgot_password"
+      exact
+      component={ForgotPasswordPage}
+    />
+    <GuestRoute
+      location={location}
+      path="/reset_password/:token"
+      exact
+      component={ResetPasswordPage}
+    />*/}
+    <UserRoute  path="/dashboard" exact component={DashboardPage} />
+    {/*<UserRoute
+      location={location}
+      path="/books/new"
+      exact
+      component={NewBookPage}
+    />*/}
+  </div>
+);
 
-function App() {
-  return (
-    <div className="ui container">
-      <Route path="/" exact component={HomePage} />
-      <Route path="/login" exact component={LoginPage} />
-      <Route path="/dashboard" exact component={DashboardPage} />
-    </div>
-  );
-}
+// App.propTypes = {
+//   location: PropTypes.shape({
+//     pathname: PropTypes.string.isRequired
+//   }).isRequired,
+//   isAuthenticated: PropTypes.bool.isRequired
+// };
 
+// function mapStateToProps(state) {
+//   return {
+//     isAuthenticated: !!state.user.email
+//   };
+// }
+
+// export default connect(mapStateToProps)(App);
 export default App;
