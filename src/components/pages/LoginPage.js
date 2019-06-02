2624fa57f2dc5afd5bbca8a88ca1234d2151d438
Xiong <xiong@ruis-mbp.attlocal.net>
2019-06-01 17:47:55 -0700

    login form with client-side validation

--- src/components/pages/LoginPage.js
+++ src/components/pages/LoginPage.js
@@ -1,11 +1,32 @@
-import React from 'react';
+import React from "react";
+// import PropTypes from "prop-types";
+// import { connect } from "react-redux";
+// import { Link } from "react-router-dom";
+import LoginForm from "../forms/LoginForm";
+// import { login } from "../../actions/auth";
 
-function LoginPage() {
-  return (
-    <div>
-      <h1>Login Page</h1>
-    </div>
-  );
+class LoginPage extends React.Component {
+  submit = data =>
+    console.log(data);
+
+  render() {
+    return (
+      <div>
+        <h1>Login page</h1>
+
+        <LoginForm submit={this.submit} />
+
+        {/*<Link to="/forgot_password">Forgot Password?</Link>*/}
+      </div>
+    );
+  }
 }
+
+// LoginPage.propTypes = {
+//   history: PropTypes.shape({
+//     push: PropTypes.func.isRequired
+//   }).isRequired,
+//   login: PropTypes.func.isRequired
+// };
 
 export default LoginPage;
