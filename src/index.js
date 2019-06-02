d29da24ab8b35732ae15d10b001b9e70d1b3526e
Xiong <xiong@ruis-mbp.attlocal.net>
2019-06-02 10:56:16 -0700

    update confirm

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import "semantic-ui-css/semantic.min.css";
 import { createStore, applyMiddleware } from "redux";
 import { Provider } from "react-redux";
 import thunk from "redux-thunk";
-// import decode from "jwt-decode";
+import decode from "jwt-decode";
 import { composeWithDevTools } from "redux-devtools-extension";
 import App from "./App";
 import * as serviceWorker from './serviceWorker';
@@ -20,11 +20,11 @@ const store = createStore(
 );
 
 if (localStorage.bookwormJWT) {
-  // const payload = decode(localStorage.bookwormJWT);
+  const payload = decode(localStorage.bookwormJWT);
   const user = {
     token: localStorage.bookwormJWT,
-    // email: payload.email,
-    // confirmed: payload.confirmed
+    email: payload.email,
+    confirmed: payload.confirmed
   };
   // setAuthorizationHeader(localStorage.bookwormJWT);
   store.dispatch(userLoggedIn(user));
