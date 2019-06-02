7e7f26c88a0e5162ad9b3fc5a07e4cb3acd05a97
Xiong <xiong@ruis-mbp.attlocal.net>
2019-06-02 16:39:30 -0700

    set up search book card and create request

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ import ConfirmationPage from "./components/pages/ConfirmationPage";
 import ForgotPasswordPage from "./components/pages/ForgotPasswordPage";
 import ResetPasswordPage from "./components/pages/ResetPasswordPage";
 import TopNavigation from "./components/navigation/TopNavigation";
-// import NewBookPage from "./components/pages/NewBookPage";
+import NewBookPage from "./components/pages/NewBookPage";
 
 const App = ({ location, isAuthenticated }) => (
   <div className="ui container">
@@ -24,7 +24,7 @@ const App = ({ location, isAuthenticated }) => (
     <Route location={location} path="/confirmation/:token" exact component={ConfirmationPage} />
     <GuestRoute location={location} path="/forgot_password" exact component={ForgotPasswordPage} />
     <GuestRoute location={location} path="/reset_password/:token" exact component={ResetPasswordPage} />
-    {/*<UserRoute location={location} path="/books/new" exact component={NewBookPage} />*/}
+    <UserRoute location={location} path="/books/new" exact component={NewBookPage} />
   </div>
 );
 
